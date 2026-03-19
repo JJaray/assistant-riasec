f0ec3cd45a517def6ef571631c62cf230d3a674e
Jacques Jaray <jacques.jaray@orange.fr>
2026-03-19 15:01:12 +0100

    ajout questionnaire RIASEC fonctionnel

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print("Projet RIASEC") 
+import json
+
+with open("../data/questions_riasec.json", "r", encoding="utf-8") as f:
+    questions = json.load(f)
+
+scores = {"R":0,"I":0,"A":0,"S":0,"E":0,"C":0}
+
+for q in questions:
+    rep = input(q["question"] + " (o/n) : ")
+    if rep.lower() == "o":
+        scores[q["type"]] += 1
+
+print("\nVotre profil RIASEC :")
+for k, v in scores.items():
+    print(k, ":", v)
+    
